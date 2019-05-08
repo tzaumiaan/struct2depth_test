bcf70aa02d309be455b3f5e0a387fba2b058881c
tzaumiaan <tzaumiaan@gmail.com>
2019-05-08 11:57:08 +0200

    update

--- inference_racecar.sh
+++ inference_racecar.sh
@@ -2,8 +2,8 @@
 tf_model_root=~/workspace/models/research/struct2depth
 
 input_dir=racecar/img/041
-output_dir=output_racecar_0508_30080
-model_checkpoint=model_ckpt_bak/0508/model-30080
+output_dir=output_racecar_0508_15980
+model_checkpoint=model_ckpt_bak/0508/model-15980
 #model_checkpoint="./model_kitti/model-199160"
 
 python $tf_model_root/inference.py \
